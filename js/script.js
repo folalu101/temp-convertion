1b03ae1cf338b2fed8a53943904bf9f14b053106
A.Adefolalu B <adefolalucode101@gmail.com>
2022-05-15 20:38:45 +0100

    create a function to convert fahrenheit to celcius

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//write a function to convert celsius to fahrenheit
+
+// function convertF(celsius){
+
+//     var fahrenheit = celsius * 9/5  + 32;
+
+//     return fahrenheit;
+// }
+// console.log(convertF(30));
+// document.write(convertF(30));
+// alert(convertF(30));
+
+
+
+//write a function to convert fahrenheit to celsius 
+function convertC(fahrenheit){
+    var celsius = fahrenheit - 32 * 5/9;
+
+    return celsius;
+}
+console.log(convertC(30));
+document.write(convertC(30));
+alert(convertC(30));
+
+
+
